12b8586d991a512a6a06152529b44ad247113b50
Bishnu Raj KC <157229290+Bishnurajkc-github@users.noreply.github.com>
2026-01-29 19:15:33 +0545

    Update app.py
    flask code

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def blog_home():
+    posts = [
+        {
+            "title": "My First Blog",
+            "content": "This is my first Flask blog post.",
+            "author": "Bishnu"
+        },
+        {
+            "title": "Why I am learning Flask",
+            "content": "Flask is simple, powerful and perfect for small projects.",
+            "author": "Bishnu"
+        }
+    ]
+    return render_template("blog/index.html", posts=posts)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
